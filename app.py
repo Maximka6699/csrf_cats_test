ef00f5d2d46be5458c19589a36bd2be17534d19d
maximka6699 <ma34lom@gmail.com>
2025-07-20 16:04:23 +0300

    хост

--- app.py
+++ app.py
@@ -166,4 +166,4 @@ def set_cat():
     return render_template_string(set_template)
 
 if __name__ == '__main__':
-    app.run(host = "127.0.0.1", port=5050 , debug=True)
+    app.run(host = "0.0.0.0", port=5050 , debug=True)
